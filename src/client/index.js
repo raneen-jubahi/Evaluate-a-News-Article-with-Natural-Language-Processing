34ac3d7c86da67e517cabbd622517d4aa6d2e2fc
RANEEN ZIYAD <raneenziyad91@gmail.com>
2024-08-29 05:28:13 +0300

    I have added the basic files for the article evaluation project.

---
+++ src/client/index.js
@@ -0,0 +1,9 @@
+import "./styles/base.scss"
+import "./styles/footer.scss"
+import "./styles/form.scss"
+import "./styles/header.scss"
+import "./styles/resets.scss"
+import "./styles/style.scss"
+import "./styles/buttons.scss"
+
+
